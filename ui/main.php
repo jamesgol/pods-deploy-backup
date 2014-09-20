d5f0336f176c5a2bbf14cea45bfe6b329f9a5271
Josh Pollock <jpollock412@gmail.com>
2014-09-20 18:18:33 -0400

    Add key and token to UI

--- ui/main.php
+++ ui/main.php
@@ -8,6 +8,18 @@
 			<label for="remote-url">Remote URL:</label>
 			<input type="text" class="" name="remote-url" id="remote-url" value="">
 		</p>
+
+		<p>
+			<label for="public-key">public-key</label>
+			<input type="text" class="" name="public-key" id="public-key" value="">
+		</p>
+
+		<p>
+			<label for="request-token">request-token:</label>
+			<input type="text" class="" name="request-token" id="request-token" value="">
+		</p>
+
+
 		<p class="submit">
 			<input type="submit" class="button button-primary" name="pods-deploy-submit" value="Deploy">
 		</p>
